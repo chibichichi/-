a212fa0207566843c9bddd801c8df13e03a259f0
Chibi <chibiriea@gmail.com>
2019-07-21 14:46:15 +0900

    유니티 파일 추가
    유니티 파일 및 테스트 파일 추가

---
+++ Unity Files/Assets/clicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clicker : MonoBehaviour
+{
+  public UnityEngine.UI.Text Point;
+  public int point = 0; //현재 포인트를 알려줌
+  public int pointperclick = 1;  // 클릭 당 얻는 포인트를 알려줌
+
+  void Update() {
+    Point.text = "획득 포인트 : " + point;
+  }
+
+  public void clicked() {
+    point += pointperclick;
+  }
+}
