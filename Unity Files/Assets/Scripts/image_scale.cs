a229fe4953104ad0c69d00af97558f5c1d19def0
korYuSa <rtgh64@gmail.com>
2019-08-03 14:22:24 -0500

    로딩 이미지 스케일링
    image_scale 스크립트 추가(main loading의 button에 적용)
    main loading 씬의 캔버스 설정 변경

---
+++ Unity Files/Assets/Scripts/image_scale.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class image_scale : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    private float imgw, imgh, imgr, scnr;
+    void Start()
+    {
+        imgw = transform.gameObject.GetComponent<RectTransform>().rect.width;
+        imgh = transform.gameObject.GetComponent<RectTransform>().rect.height;
+        imgr = imgh/imgw;
+        scnr = Screen.height/Screen.width;
+        Vector2 myVector = new Vector2();
+        if(imgr >= scnr)
+        {
+            myVector = new Vector2(Screen.width,Screen.width*imgr);
+        }
+        else
+        {
+            myVector = new Vector2(Screen.height/imgr,Screen.height);
+        } 
+    
+        transform.gameObject.GetComponent<RectTransform>().sizeDelta = myVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
