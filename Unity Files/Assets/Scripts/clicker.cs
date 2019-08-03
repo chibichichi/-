dc59a13d1c77d166da57a7c16f008e61029d0544
Chibi <chibiriea@gmail.com>
2019-08-03 20:03:34 +0900

    Unity Files
    무언가 모르겠는 잡다한 유니티파일

---
+++ Unity Files/Assets/Scripts/clicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clicker : MonoBehaviour
+{
+  public UnityEngine.UI.Text Point;
+  public int point = 0; //현재 포인트를 알려줌
+  public int pointperclick = 50;  // 클릭 당 얻는 포인트를 알려줌
+
+  void Update() {
+    Point.text = "획득 쥬얼 : " + point;
+  }
+
+  public void clicked() {
+    point += pointperclick;
+  }
+}
---
+++ Unity Files/Assets/Scripts/scene_change.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_change : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void clicked()
+   {
+     SceneManager.LoadScene("clicker_test");
+   }
+}
